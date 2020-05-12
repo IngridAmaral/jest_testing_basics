2abd144259c8348265d70f21232d5b28dddcb38a
Ingrid <guid_95@hotmail.com>
2020-05-12 15:04:22 +0200

    toBe, not.~, toBeLessThan, toEqual, toMatch, toBeNull, toBeFalsy, toContain

--- functions.js
+++ functions.js
@@ -1,5 +1,12 @@
 const functions = { 
     add: (num1, num2) => num1+ num2,
+    isNull: () => null,
+    checkValue: (x) => x,
+    createUser: () => {
+        const user = { firstName: 'Ingrid'}
+        user['lastName'] = 'Amaral';
+        return user;
+    }
 }
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,39 @@ const functions = require('./functions');
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+test('Should be false', () => {
+    expect(functions.checkValue(null)).toBeFalsy();  //0, null, undefined, '', NaN, false
+});
+
+test('User should be Ingrid Amaral object', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Ingrid', 
+        lastName:'Amaral'
+    });  
+    //toBe matcher is for basically primitive types such as: number, string, boolean, null, undefined, symbol
+    //so for objects and arrays the matcher should be toEqual instead
+});
+
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+})
+
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/i/i);
+})
+
+test('Admin should be in usernames', () => {
+    const userNames = ['John', 'Karen', 'Admin'];
+    expect(userNames).toContain('Admin')
+})
